784de78196d509cdb3d33a1692eb2b043b2c5bfd
IgorSednyov <azru-maz@mail.ru>
2019-11-23 19:28:53 +0300

    include php-view

--- public/index.php
+++ public/index.php
@@ -3,7 +3,14 @@
 require '__DIR__v' . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
