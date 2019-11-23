f82c41ccbd621a58750647827dbef6eeca2c1870
IgorSednyov <azru-maz@mail.ru>
2019-11-23 18:59:24 +0300

    ad route "course"

--- public/index.php
+++ public/index.php
@@ -10,12 +10,12 @@ $app->addErrorMiddleware(true, true, true);
 $app->get('/', function ($request, $response) {
     return $response->write('Welcome to Slim!');
 });
-$app->get('/users', function ($request, $response) {
-    return $response->write('GET /users');
-});
-
 $app->post('/users', function ($request, $response) {
-    return $response->write('POST /users');
+    return $response->withStatus(302);
+});
+$app->get('/courses/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    return $response->write("Course id: {$id}");
 });
 $app->run();
 ?>
